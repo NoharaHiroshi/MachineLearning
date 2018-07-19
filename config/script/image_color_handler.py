2d3786863ca7f3b9e1f79467b2f08ff9ba7c5751
Lands <380788433@qq.com>
2018-07-19 16:27:05 +0800

    第一次测试： 检测文件数72，匹配率30， 准确率41.6%

--- config/script/image_color_handler.py
+++ config/script/image_color_handler.py
@@ -108,6 +108,8 @@ def open_image(file_name):
         pass
 
 if __name__ == '__main__':
+    # 记录
+    # 第一次测试： 检测文件数72，匹配率30， 准确率41.6%
     d = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
     img_file_dir = os.path.join(d, u'sample\image_color')
     all_file = os.listdir(img_file_dir)
