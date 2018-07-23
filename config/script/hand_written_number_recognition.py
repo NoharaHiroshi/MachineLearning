784d64fe3a376f951fee0b23ce9c3ba063ff0246
Lands <380788433@qq.com>
2018-07-23 16:49:48 +0800

    手写数字识别

--- config/script/hand_written_number_recognition.py
+++ config/script/hand_written_number_recognition.py
@@ -5,10 +5,12 @@ from PIL import Image
 from numpy import *
 
 
+# 图片转换向量
 def img2vector(filename):
     base_dir = '\\'.join(os.path.abspath(__file__).split('\\')[:2])
     file_path = '\\'.join([base_dir, r'sample\number_data'])
     full_path = '\\'.join([file_path, filename])
+    # 1行1024列
     vector = zeros((1, 1024))
     f = open(full_path)
     for i in range(32):
@@ -17,7 +19,18 @@ def img2vector(filename):
             vector[0, 32*i+j] = int(line_str[j])
     return vector
 
+
+def hand_writing():
+    base_dir = '\\'.join(os.path.abspath(__file__).split('\\')[:2])
+    file_path = '\\'.join([base_dir, r'sample\number_data'])
+    hw_file_list = os.listdir(file_path)
+    m = len(hw_file_list)
+    for i in range(m):
+        file_name_str = hw_file_list[i]
+        num_type = file_name_str.split('_')[0]
+        vector = img2vector(file_name_str)
+        print vector
+
+
 if __name__ == '__main__':
-    ver = img2vector('0_0.txt')
-    for i in ver:
-        print i
+    pass
