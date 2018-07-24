be4b5a2989074bd4c9f33d7cf605563e6d2d9c80
Lands <380788433@qq.com>
2018-07-24 15:28:01 +0800

    好吧，数组之前没使用过，有些东西看的莫名其妙，先学习下数组

--- config/script/hand_written_number_recognition.py
+++ config/script/hand_written_number_recognition.py
@@ -7,12 +7,9 @@ from numpy import *
 
 # 图片转换向量
 def img2vector(filename):
-    base_dir = '\\'.join(os.path.abspath(__file__).split('\\')[:2])
-    file_path = '\\'.join([base_dir, r'sample\test'])
-    full_path = '\\'.join([file_path, filename])
     # 1行1024列
     vector = zeros((1, 1024))
-    f = open(full_path)
+    f = open(filename)
     for i in range(32):
         line_str = f.readline()
         for j in range(32):
@@ -47,7 +44,15 @@ def hand_writing_class(in_x, data_set, labels, k):
     # array([[0, 1, 2],
     #        [0, 1, 2]])
     diff_mat = tile(in_x, (data_set_size, 1)) - data_set
+    # ** 代表指数运算
     sq_diff_mat = diff_mat**2
+    # c = np.array([[0, 2, 1], [3, 5, 6], [0, 1, 1]])
+    # print c.sum()
+    # print c.sum(axis=0)
+    # print c.sum(axis=1)
+    # 结果分别是：19, [3 8 8], [ 3 14  2]
+    # axis=0, 表示列。
+    # axis=1, 表示行。
     sq_distances = sq_diff_mat.sum(axis=1)
     distances = sq_distances**0.5   # 欧氏距离开平方
     sorted_dist_ind = distances.argsort()  # 距离排序的索引排序
@@ -65,25 +70,30 @@ def hand_writing():
     file_path = '\\'.join([base_dir, r'sample\test'])
     hw_file_list = os.listdir(file_path)
     m = len(hw_file_list)
+    training_mat = zeros([0, 1024])
+    labels = list()
     for i in range(m):
         file_name_str = hw_file_list[i]
         full_file_path = os.path.join(file_path, file_name_str)
         num_type = file_name_str.split('_')[0]
-        f = open(full_file_path)
-        f_line = f.readlines()
-        for l in f_line:
-            l = l.replace('\n', '')
-            print l
-
+        labels.append(num_type)
+        vector = img2vector(full_file_path)
+        training_mat[i, :] = vector
+    test_file_path = '\\'.join([base_dir, r'sample\number_data'])
+    test_file_list = os.listdir(test_file_path)
+    error_count = 0.0
+    m_test = len(test_file_list)
+    for i in range(m_test):
+        test_file_name_str = test_file_list[i]
+        test_full_file_path = os.path.join(file_path, test_file_name_str)
+        test_num_type = test_file_name_str.split('_')[0]
+        test_vector = img2vector(test_full_file_path)
+        classify_result = hand_writing_class(test_vector, training_mat, labels, 3)
+        print 'the classify result is %s, the real answer is %s' % (classify_result, test_num_type)
+        if classify_result != test_num_type:
+            error_count += 1.0
+        print error_count
 
 if __name__ == '__main__':
-    base_dir = '\\'.join(os.path.abspath(__file__).split('\\')[:2])
-    file_path = '\\'.join([base_dir, r'sample\test'])
-    name = '0_0.txt'
-    sample_name = '0_1.txt'
-    full_file_path = os.path.join(file_path, name)
-    sample_file_path = os.path.join(file_path, sample_name)
-    f = open(full_file_path)
-    sample_f = open(sample_file_path)
-    in_x = img2vector(f)
-    data_set = img2vector(f)
+    hand_writing()
+
