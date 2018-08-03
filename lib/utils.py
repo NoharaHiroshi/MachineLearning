1602e75d04f75efcf2bf7f10542a88c9b478f3f7
Lands <380788433@qq.com>
2018-08-03 16:30:29 +0800

    生成决策树

--- lib/utils.py
+++ lib/utils.py
@@ -48,17 +48,17 @@ def split_data_set(data_set, axis, value):
 
 
 def choose_best_feature_to_split(data_set):
-    print u'原始数据集：%s' % data_set
+    # print u'原始数据集：%s' % data_set
     # 获取数据的特征值个数，-1是标签
     num_features = len(data_set[0]) - 1
     # 计算了整个数据集的原始香农熵，保存最初的无序度量值，用于与划分后的数据集进行熵值的比较
     base_entropy = cal_shannon_ent(data_set)
-    print u'原始香农熵值：%s' % base_entropy
+    # print u'原始香农熵值：%s' % base_entropy
     base_info_gain = 0.0
     # 最利于划分数据的特征，默认-1
     best_feature = -1
     for i in range(num_features):
-        print u'************************当前特征：%s************************' % i
+        # print u'************************当前特征：%s************************' % i
         # 特征值列表
         feat_list = [example[i] for example in data_set]
         # 去重
@@ -66,9 +66,9 @@ def choose_best_feature_to_split(data_set):
         new_entropy = 0.0
         # 循环标签
         for value in unique_values:
-            print u'——————————————————当前特征值：%s——————————————————————' % value
+            # print u'——————————————————当前特征值：%s——————————————————————' % value
             sub_data_set = split_data_set(data_set, i, value)
-            print sub_data_set
+            # print sub_data_set
             # 当前标签出现的概率
             prob = len(sub_data_set) / float(len(data_set))
             shannon_ent = cal_shannon_ent(sub_data_set)
@@ -76,13 +76,51 @@ def choose_best_feature_to_split(data_set):
             new_entropy += prob * shannon_ent
         # 划分完一个特征，计算香农熵值是否减少及减少量，减少的越多，越有序
         info_gain = base_entropy - new_entropy
-        print u'香农熵值： %s' % info_gain
+        # print u'香农熵值： %s' % info_gain
         if info_gain > base_info_gain:
             base_info_gain = info_gain
             best_feature = i
     return best_feature
 
+
+# 返回出现次数最多的值
+def majority_cnt(class_list):
+    class_count = dict()
+    for vote in class_list:
+        if vote not in class_count:
+            class_count[vote] = 1
+        else:
+            class_count[vote] += 1
+    sorted_class_count = sorted(class_count.items(), key=lambda x: x[-1], reverse=True)
+    return sorted_class_count[0][0]
+
+
+def create_tree(data_set, labels):
+    tree = dict()
+    # 最优划分的特征索引
+    best_feature = choose_best_feature_to_split(data_set)
+    # 最优划分的特征标签
+    best_feature_label = labels[best_feature]
+    tree[best_feature_label] = dict()
+    second_tree = tree[best_feature_label]
+    # 特征值set
+    feature_value_set = set([data[best_feature] for data in data_set])
+    # 遍历每一个特征值，计算当前特征值
+    for f_v in feature_value_set:
+        last_data_set = split_data_set(data_set, best_feature, f_v)
+        last_data_label_list = [last_data[-1] for last_data in last_data_set]
+        # 当前分支划分的数据集中只有一个标签，则结束
+        if len(set(last_data_label_list)) == 1:
+            second_tree[f_v] = last_data_label_list[0]
+        # 继续划分
+        else:
+            # 剔除最优划分的特征标签
+            labels.remove(best_feature_label)
+            _tree = create_tree(last_data_set, labels)
+            second_tree[f_v] = _tree
+    return tree
+
 if __name__ == '__main__':
     d, l = create_data_set()
-    r = choose_best_feature_to_split(d)
-    print r
+    print d, l
+    print create_tree(d, l)
